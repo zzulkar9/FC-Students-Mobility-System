8a968852d83524482291f5c08adbd8b716bd5c77
zzulkar9 <muhdzzulkar9@gmail.com>
2024-04-12 15:46:12 +0800

    Updated Application Form

--- app/Http/Controllers/ApplicationFormController.php
+++ app/Http/Controllers/ApplicationFormController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User; // Import the User model.
 
 class ApplicationFormController extends Controller
 {
     public function index()
     {
-        // You can perform additional checks or pass data to the view here
-        return view('application-form.index');
+        // Define the user types based on your constants in User model
+        $userTypes = [
+            User::TYPE_UTM_STUDENT => 'UTM Student',
+            User::TYPE_OTHER_STUDENT => 'Other University Student',
+            User::TYPE_ADMIN => 'Admin',
+            User::TYPE_TDA => 'TDA',
+            User::TYPE_PROGRAM_COORDINATOR => 'Program Coordinator',
+            User::TYPE_UTM_STAFF => 'UTM Staff',
+        ];
+
+        // Pass the user types to the view
+        return view('application-form.index', compact('userTypes'));
     }
 }
